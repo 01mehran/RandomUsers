36702fb349727fe473cd12a0d95bf88078f365ca
01mehran <132054519+01mehran@users.noreply.github.com>
2024-07-31 00:00:03 +0330

    Done the styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,20 @@ module.exports = {
   theme: {
     extend: {
       width: {
-        wrapperWidht : "700px"
-      }
+        wrapperWidht: "800px",
+        address: "500px",
+      },
+      colors: {
+        bgColor: "aliceblue",
+        blue: "#43d7ff",
+        dark: "#3f4954",
+        darker: "#26292",
+        lightgreen: "#83ba43",
+      },
+      fontSize: {
+        text: "19px",
+      },
     },
   },
   plugins: [],
-}
+};
