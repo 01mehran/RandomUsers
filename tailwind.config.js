22c59255a5e2d7c9839011473f1de818ec01da24
01mehran <132054519+01mehran@users.noreply.github.com>
2024-07-30 16:00:14 +0330

    Create html docs & files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        wrapperWidht : "700px"
+      }
+    },
   },
   plugins: [],
 }
